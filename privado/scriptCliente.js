8e8001585eed4d49284db035657bf582e24a1dc1
RobertoVeneziani <robertovenezianicelular@hotmail.com>
2024-03-26 22:26:38 -0300

    atualizar

--- privado/scriptCliente.js
+++ privado/scriptCliente.js
@@ -1,6 +1,7 @@
 
 const formularioCliente = document.getElementById('formCliente');
 formularioCliente.onsubmit = validarFormulario;
+window.onload = buscarClientes;
 
 function validarFormulario(evento){
     if (formularioCliente.checkValidity()){
@@ -43,6 +44,7 @@ function cadastrarCliente(cliente){
         if (dados.status){
             formularioCliente.reset();
             mostrarMensagem(dados.mensagem, true);
+            buscarClientes();
         }
         else{
             mostrarMensagem(dados.mensagem, false);
@@ -51,6 +53,25 @@ function cadastrarCliente(cliente){
     .catch((erro) =>{
         mostrarMensagem(erro.mensage, false)
     });
+}
+
+
+function buscarClientes(){
+    fetch('http://localhost:3000/clientes',{method:  'GET'})
+    .then(responsta=>{
+        return resposta.json();
+    })
+    .then((dados)=>{
+        if (Arrey.isArrey(dados)){
+            exibirTabelaClientes(dados);    
+        }
+        else{
+            mostrarMensagem(dados.mensagem, false); 
+        }      
+    }) 
+    .catch ((erro)=> {
+        mostrarMensagem(erro.message, false);
+    })     
 }
 
 function mostrarMensagem(mensagem, sucesso = false){
@@ -81,56 +102,87 @@ function exibirTabelaClientes(listaClientes){
     espacoTabela.innerHTML = '';
     if (listaClientes.length > 0){
         const tabela = document.createElement('table');
-        tabela.classList.add(['table', 'table-striped', 'table-hover']);
+        tabela.className = 'table table-striped table-hover';
         const cabecalho = document.createElement('thead');
         cabecalho.innerHTML = 
-        <tr>
-            <th>#</th>
-            <th>evento</th>
-            <th>data</th>
-            <th>local</th>
-            <th>quantidade</th>
-            <th>preco</th>
-            <th>cpf</th>
-            <th>nome</th>
-            <th>endereco</th>
-            <th>cidade</th>
-            <th>estado</th>
-            <th>telefone</th>
-            <th>email</th>
-            <th>ações</th>
-        </tr>
-        ;
+            <tr>
+                <th>#</th>
+                <th>evento</th>
+                <th>data</th>
+                <th>local</th>
+                <th>quantidade</th>
+                <th>preco</th>
+                <th>cpf</th>
+                <th>nome</th>
+                <th>endereco</th>
+                <th>cidade</th>
+                <th>estado</th>
+                <th>telefone</th>
+                <th>email</th>
+                <th>ações</th>
+            </tr>
+            ;
         tabela.appendChild(cabecalho);
         const corpo = document.createElement('tbody');
         for (let i = 0; i < listaClientes.length; i++){
             const cliente = listaClientes[i];
             const linha = document.createElement('tr');
             linha.innerHTML = `
-            <td>${cliente.codigo}</td>
-            <td>${cliente.evento}</td>
-            <td>${cliente.data}</td>
-            <td>${cliente.local}</td>
-            <td>${cliente.quantidade}</td>
-            <td>${cliente.preco}</td>
-            <td>${cliente.cpf}</td>
-            <td>${cliente.nome}</td>
-            <td>${cliente.endereco}</td>
-            <td>${cliente.cidade}</td>
-            <td>${cliente.estado}</td>
-            <td>${cliente.telefone}</td>
-            <td>${cliente.email}</td>
-            <td>
-                <button>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+                <td>${cliente.codigo}</td>
+                <td>${cliente.evento}</td>
+                <td>${cliente.data}</td>
+                <td>${cliente.local}</td>
+                <td>${cliente.quantidade}</td>
+                <td>${cliente.preco}</td>
+                <td>${cliente.cpf}</td>
+                <td>${cliente.nome}</td>
+                <td>${cliente.endereco}</td>
+                <td>${cliente.cidade}</td>
+                <td>${cliente.estado}</td>
+                <td>${cliente.telefone}</td>
+                <td>${cliente.email}</td>
+                <td>
+                    <button onclick="selecionarCliente('${cliente.codigo}',
+                                                        '${cliente.evento}',
+                                                        '${cliente.data}',
+                                                        '${cliente.local}',
+                                                        '${cliente.quantidade}',
+                                                        '${cliente.cpf}',
+                                                        '${cliente.nome}';
+                                                        '${cliente.endereco}',
+                                                        '${cliente.bairro}',
+                                                        '${cliente.cidade}',
+                                                        '${cliente.estado}',
+                                                        '${cliente.telefone}',
+                                                        '${cliente.email}')">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
-                    </svg>
-                </button>        
-            </td>`;
-        corpo.appendChild(linha);
+                        </svg>
+                    </button>        
+                </td>`;
+            corpo.appendChild(linha);
         }
+        tabela.appendChild(corpo);
+        espacoTabela.appendChild(tabela);
     }
     else{
-        espacoTabela.innerHTML = "Nenhum cliente cadastrado.";
+        espacoTabela.innerHTML = '<p>Nenhum cliente cadastrado!</p>';
     }
+}
+
+function selecionarCliente(codigo, evento, data, local, quantidade, preco, cpf, nome, endereco, bairro, cidade, estado, telefone, email){
+    document.getElementById('codigo').value = codigo;
+    document.getElementById('evento').value = evento;
+    document.getElementById('data').value = data;
+    document.getElementById('local').value= local;
+    document.getElementById('quantidade').value = quantidade;   
+    document.getElementById('preco').value = preco;
+    document.getElementById('cpf').value = cpf;
+    document.getElementById('nome').value = nome;
+    document.getElementById('endereco').value = endereco;
+    document.getElementById('bairro').value= bairro;
+    document.getElementById('cidade').value = cidade;   
+    document.getElementById('estado').value = estado;
+    document.getElementById('telefone').value = telefone;   
+    document.getElementById('email').value = email;
 }
